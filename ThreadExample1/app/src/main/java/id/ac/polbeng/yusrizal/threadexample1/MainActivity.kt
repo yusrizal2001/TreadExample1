657ba5a05b3147f36200b288784136c43db38b9f
yusrizal10 <79837411+yusrizal2001@users.noreply.github.com>
2022-05-20 11:04:38 +0700

    Add files via upload

---
+++ ThreadExample1/app/src/main/java/id/ac/polbeng/yusrizal/threadexample1/MainActivity.kt
@@ -0,0 +1,25 @@
+package id.ac.polbeng.yusrizal.threadexample1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun startThread(view: View):Unit{
+        for (i in 1..10) {
+            Log.d(TAG, "startThread: $i")
+            try {
+                Thread.sleep(1000)
+            } catch (ie: InterruptedException){
+                ie.printStackTrace()
+            }
+        }
+
+    }
+}
